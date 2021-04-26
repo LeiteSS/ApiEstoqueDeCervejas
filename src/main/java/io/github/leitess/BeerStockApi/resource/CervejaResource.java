ec7a366df232564501869fc5ceb1495e4551ad07
Silas Santos Leite <estoudente.program@gmail.com>
2021-04-26 13:50:50 -0300

    Cross Origin

--- src/main/java/io/github/leitess/BeerStockApi/resource/CervejaResource.java
+++ src/main/java/io/github/leitess/BeerStockApi/resource/CervejaResource.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -24,6 +25,7 @@ import javax.validation.Valid;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/api/v1")
 @AllArgsConstructor(onConstructor = @__(@Autowired))
 public class CervejaResource implements CervejaResourceDocs {
